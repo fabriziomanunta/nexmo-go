8dca1b5db583d86b7b839dbb6237a23aeb5fd670
Fabrizio Manunta <fabrizio.manunta@gmail.com>
2019-10-30 12:09:47 +0100

    Change structure of 'From' field for CallRequest again, back to map of strings

--- structs.go
+++ structs.go
@@ -74,12 +74,8 @@ type SendSMSResponseMessage struct {
 }
 
 type CreateCallRequest struct {
-	To   []interface{} `json:"to"`
-	From struct {
-		Type       string `json:"type"`
-		Number     string `json:"number,omitempty"`
-		DtmfAnswer string `json:"dtmfAnswer,omitempty"`
-	} `json:"from"`
+	To               []interface{}       `json:"to"`
+	From             map[string]string   `json:"from"`
 	Ncco             []map[string]string `json:"ncco"`
 	AnswerURL        []string            `json:"answer_url"`
 	AnswerMethod     string              `json:"answer_method,omitempty"`
